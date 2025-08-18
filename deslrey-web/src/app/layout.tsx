83ccce2fafbbcc1fc7364e9a23c93881fe67354f
Deslre <2135232170@qq.com>
2025-08-18 09:59:40 +0800

    feat: react19 compatible

--- deslrey-web/src/app/layout.tsx
+++ deslrey-web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import type { Metadata } from "next";
+import "@ant-design/v5-patch-for-react-19";
 
 export const metadata: Metadata = {
     title: "deslrey",
