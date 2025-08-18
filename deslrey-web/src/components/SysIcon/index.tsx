6fd33ae6fa72a2d8db63178c4d7090df8e4c88d6
Deslre <2135232170@qq.com>
2025-08-18 19:42:25 +0800

    feat: iconfont

---
+++ deslrey-web/src/components/SysIcon/index.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { createFromIconfontCN } from "@ant-design/icons";
+
+const SysIcon = createFromIconfontCN({
+    scriptUrl: ["//at.alicdn.com/t/c/font_5001725_3pt18660b44.js"],
+});
+
+export default SysIcon;
