2d379806a5627c7ccce7607d6dadcf1e301ce464
Deslre <2135232170@qq.com>
2025-08-20 15:01:34 +0800

    feat: nav aside

--- deslrey-web/src/components/Nav/index.tsx
+++ deslrey-web/src/components/Nav/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState, useRef } from "react";
 import styles from "./index.module.scss";
 import SysIcon from "../SysIcon";
 import { NavObj, NavList } from "./config";
 import Link from "next/link";
+import Checkbox from "../Checkbox";
 
 const NavItem: React.FC<{ item: NavObj }> = ({ item }) => {
     return (
@@ -14,11 +17,31 @@ const NavItem: React.FC<{ item: NavObj }> = ({ item }) => {
 };
 
 const Nav: React.FC = () => {
+    const [open, setOpen] = useState(false);
+    const navMainRef = useRef<HTMLDivElement>(null);
+
     return (
-        <nav className={styles.NavMain}>
-            {NavList.map((item) => (
-                <NavItem key={item.key} item={item} />
-            ))}
+        <nav className={styles.NavWrapper}>
+            {/* 移动端汉堡按钮 */}
+            <div className={styles.mobileToggle}>
+                <Checkbox open={open} onToggle={setOpen} />
+            </div>
+
+            {/* 菜单内容 */}
+            {open && (
+                <div
+                    className={styles.overlay}
+                    onClick={() => setOpen(false)}
+                />
+            )}
+            <div
+                ref={navMainRef}
+                className={`${styles.NavMain} ${open ? styles.open : ""}`}
+            >
+                {NavList.map((item) => (
+                    <NavItem key={item.key} item={item} />
+                ))}
+            </div>
         </nav>
     );
 };
