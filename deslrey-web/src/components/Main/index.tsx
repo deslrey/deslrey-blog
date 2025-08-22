b49e78ac18ef8ef7b8d90f6f392635ac1dc37466
Deslre <2135232170@qq.com>
2025-08-22 23:09:09 +0800

    feat: main

--- deslrey-web/src/components/Main/index.tsx
+++ deslrey-web/src/components/Main/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 import styles from "./index.module.scss";
+import LatestReleases from "./LatestReleases";
+import Classify from "./Classify";
+import PopularTags from "./PopularTags";
 
 const Main: React.FC = () => {
-    return <div></div>;
+    return (
+        <div className={styles.main}>
+            <div className={styles.latestReleases}>
+                <LatestReleases />
+            </div>
+            <div className={styles.classTags}>
+                <Classify />
+                <PopularTags />
+            </div>
+        </div>
+    );
 };
 
 export default Main;
