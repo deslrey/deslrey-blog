7d90d8b845531bff94da78669b2a2ba3ff3b0563
Deslre <2135232170@qq.com>
2025-08-22 23:48:01 +0800

    feat: article

--- deslrey-web/src/app/article/page.tsx
+++ deslrey-web/src/app/article/page.tsx
@@ -1,9 +1,51 @@
 import React from "react";
-
 import styles from "./article.module.scss";
+import { ArticleList } from "@/json/Article";
+import dayjs from "dayjs";
+import Link from "next/link";
 
 const Article = () => {
-    return <div>Article</div>;
+    return (
+        <div className={styles.article}>
+            <div className={styles.container}>
+                <ul className={styles.list}>
+                    {ArticleList.map((item) => (
+                        <Link
+                            key={item.id}
+                            href={`/article/${item.id}`}
+                            className={styles.item}
+                        >
+                            <div>
+                                <span className={styles.title}>
+                                    {item.title}
+                                    {item.sticky && (
+                                        <span className={styles.sticky}>
+                                            置顶
+                                        </span>
+                                    )}
+                                </span>
+                                <p className={styles.des}>{item.des}</p>
+                                <div className={styles.meta}>
+                                    <span>
+                                        {dayjs(item.createTime).format(
+                                            "YYYY-MM-DD HH:mm"
+                                        )}
+                                    </span>
+                                    {item.edit && (
+                                        <span className={styles.edit}>
+                                            已修改
+                                        </span>
+                                    )}
+                                    <span>{item.wordCount} 字</span>
+                                    <span>预计阅读 {item.readTime} 分钟</span>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Article;
