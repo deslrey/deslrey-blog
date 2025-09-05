5b757e721b7ec0f4b61d87a5e8c38a149c528f55
deslrey <deslre0381@gmail.com>
2025-09-05 15:03:44 +0800

    init java

---
+++ deslrey-java/src/main/java/org/deslrey/DeslreyJavaApplication.java
@@ -0,0 +1,13 @@
+package org.deslrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeslreyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeslreyJavaApplication.class, args);
+    }
+
+}
