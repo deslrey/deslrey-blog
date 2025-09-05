2e403944d407f649d7f7861f09086496e52c69da
deslrey <deslre0381@gmail.com>
2025-09-05 15:32:29 +0800

    feat: configuration initialization

--- deslrey-java/src/main/java/org/deslrey/DeslreyJavaApplication.java
+++ deslrey-java/src/main/java/org/deslrey/DeslreyJavaApplication.java
@@ -1,9 +1,11 @@
 package org.deslrey;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.deslrey.mapper")
 public class DeslreyJavaApplication {
 
     public static void main(String[] args) {
