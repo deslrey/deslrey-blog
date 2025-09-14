9bbba6f2b4570135845d19e2d0a244ed870652de
deslrey <deslre0381@gmail.com>
2025-09-14 23:06:09 +0800

    feat: header style

--- deslrey-admin/src/components/Header/index.tsx
+++ deslrey-admin/src/components/Header/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
 import styles from './index.module.scss'
+import { useTime } from '../../hooks/useTime'
+
+import avatar from '../../assets/images/avatar.jpg'
 
 const Header: React.FC = () => {
+
+    const { timeText } = useTime()
+
     return (
-        <div className={styles.headerBox}>Header</div>
+        <div className={styles.headerBox}>
+            <div className={styles.leftBox}>
+                <img src={avatar} alt="avatar" className={styles.avatar} />
+            </div>
+
+            <div className={styles.midBox}>
+                中间
+            </div>
+            <div className={styles.rightBox}>
+                {timeText}
+            </div>
+        </div>
     )
 }
 
