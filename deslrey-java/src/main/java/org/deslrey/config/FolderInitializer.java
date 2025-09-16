11d35907c70b137d76a6fb225dedaf126c5cc17a
deslrey <deslre0381@gmail.com>
2025-09-16 16:49:27 +0800

    feat: static resource

---
+++ deslrey-java/src/main/java/org/deslrey/config/FolderInitializer.java
@@ -0,0 +1,41 @@
+package org.deslrey.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+/**
+ * <br>
+ * 检查图片存储目录
+ * </br>
+ *
+ * @author deslrey
+ * @version 1.0
+ * @since 2025/9/16 16:27
+ */
+@Slf4j
+@Component
+public class FolderInitializer implements CommandLineRunner {
+
+
+    @Value("${custom.static-source-path}")
+    private String folderPath;
+
+    @Override
+    public void run(String... args) throws Exception {
+        File dir = new File(folderPath);
+        if (!dir.exists()) {
+            boolean created = dir.mkdirs();
+            if (created) {
+                log.info("图片目录创建成功 ======> {}", folderPath);
+            } else {
+                log.error("图片目录创建失败 ======> {}", folderPath);
+            }
+        } else {
+            log.info("图片目录已存在 ======> {}", folderPath);
+        }
+    }
+}
---
+++ deslrey-java/src/main/java/org/deslrey/config/WebConfig.java
@@ -0,0 +1,32 @@
+package org.deslrey.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * <br>
+ * 图片静态资源
+ * </br>
+ *
+ * @author deslrey
+ * @version 1.0
+ * @since 2025/9/16 16:31
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${custom.static-source-path}")
+    private String staticSourcePath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String location = "file:" + staticSourcePath;
+        if (!location.endsWith("/")) {
+            location += "/";
+        }
+        registry.addResourceHandler("/staticSource/**")
+                .addResourceLocations(location);
+    }
+}
