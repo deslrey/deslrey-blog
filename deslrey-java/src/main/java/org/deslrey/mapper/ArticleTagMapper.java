aa479a1823b2758c509be0f80977a528b5a623b7
deslrey <deslre0381@gmail.com>
2025-09-18 15:15:15 +0800

    feat: article - tag

---
+++ deslrey-java/src/main/java/org/deslrey/mapper/ArticleTagMapper.java
@@ -0,0 +1,13 @@
+package org.deslrey.mapper;
+
+/**
+ * <br>
+ * 文章 - 标签 mapper
+ * </br>
+ *
+ * @author deslrey
+ * @version 1.0
+ * @since 2025/9/15 15:54
+ */
+public interface ArticleTagMapper {
+}
