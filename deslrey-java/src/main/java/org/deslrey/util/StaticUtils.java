9eaeaa72588da879edade0e0c319658edf5bb044
deslrey <deslre0381@gmail.com>
2025-09-18 10:09:35 +0800

    feat: static utils

---
+++ deslrey-java/src/main/java/org/deslrey/util/StaticUtils.java
@@ -0,0 +1,16 @@
+package org.deslrey.util;
+
+/**
+ * <br>
+ * 存储全局常量值
+ * </br>
+ *
+ * @author deslrey
+ * @version 1.0
+ * @since 2025/9/18 9:51
+ */
+public class StaticUtils {
+
+    public static final String BASE_URL = "http://localhost:8080/deslrey/staticSource/";
+
+}
