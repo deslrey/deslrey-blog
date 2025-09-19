280427aaa040b75e76429477f4649a7c6a4e048b
deslrey <deslre0381@gmail.com>
2025-09-19 18:29:51 +0800

    feat: tag page

---
+++ deslrey-admin/src/pages/Admin/Tag/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './index.module.scss'
+
+const TagPage: React.FC = () => {
+    return (
+        <div>TagPage</div>
+    )
+}
+
+export default TagPage
