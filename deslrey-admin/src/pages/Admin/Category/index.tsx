b8a16c820048278a44503a13c73d280f51b49231
deslrey <deslre0381@gmail.com>
2025-09-19 18:29:31 +0800

    feat: category page

---
+++ deslrey-admin/src/pages/Admin/Category/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './index.module.scss'
+
+const CategoryPage: React.FC = () => {
+    return (
+        <div>Category</div>
+    )
+}
+
+export default CategoryPage
