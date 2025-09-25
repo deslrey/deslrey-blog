d0a575455ce3712bd3e26acbd77ce8ca0935ebf4
deslrey <deslre0381@gmail.com>
2025-09-25 10:18:08 +0800

    feat: header add token

--- deslrey-admin/src/utils/request.ts
+++ deslrey-admin/src/utils/request.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import type { AxiosInstance, AxiosRequestConfig } from "axios";
 import { Message } from "./message";
+import { useUserStore } from "../store";
 
 export interface Results<T = any> {
     code: number;
@@ -17,9 +18,9 @@ class Request {
         // 请求拦截器
         this.instance.interceptors.request.use(
             (config) => {
-                const token = localStorage.getItem("token");
+                const token = useUserStore.getState().user.token;
                 if (token && config.headers) {
-                    config.headers.Authorization = `Bearer ${token}`;
+                    config.headers.Authorization = `token ${token}`;
                 }
                 return config;
             },
@@ -29,9 +30,8 @@ class Request {
         // 响应拦截器
         this.instance.interceptors.response.use(
             (response) => {
-                // 只负责处理异常，不改返回类型
                 if (response.data.code !== 200) {
-                    Message.error(response.data.message)
+                    Message.error(response.data.message);
                     return Promise.reject(response.data);
                 }
                 return response;
@@ -41,29 +41,42 @@ class Request {
     }
 
     // GET
-    public async get<T = any>(url: string, config?: AxiosRequestConfig): Promise<Results<T>> {
+    public async get<T = any>(
+        url: string,
+        config?: AxiosRequestConfig
+    ): Promise<Results<T>> {
         const res = await this.instance.get<Results<T>>(url, config);
         return res.data;
     }
 
     // POST
-    public async post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<Results<T>> {
+    public async post<T = any>(
+        url: string,
+        data?: any,
+        config?: AxiosRequestConfig
+    ): Promise<Results<T>> {
         const res = await this.instance.post<Results<T>>(url, data, config);
         return res.data;
     }
 
     // PUT
-    public async put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<Results<T>> {
+    public async put<T = any>(
+        url: string,
+        data?: any,
+        config?: AxiosRequestConfig
+    ): Promise<Results<T>> {
         const res = await this.instance.put<Results<T>>(url, data, config);
         return res.data;
     }
 
     // DELETE
-    public async delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<Results<T>> {
+    public async delete<T = any>(
+        url: string,
+        config?: AxiosRequestConfig
+    ): Promise<Results<T>> {
         const res = await this.instance.delete<Results<T>>(url, config);
         return res.data;
     }
-
 }
 
 const request = new Request({
