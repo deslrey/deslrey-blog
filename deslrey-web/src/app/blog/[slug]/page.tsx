00a4eb4b9f41e7df7ba8f773f42c8dcc44dd789a
deslrey <deslre0381@gmail.com>
2025-10-21 16:12:23 +0800

    feat: blog page use client

--- deslrey-web/src/app/blog/[slug]/page.tsx
+++ deslrey-web/src/app/blog/[slug]/page.tsx
@@ -1,44 +1,7 @@
-import { BytemdViewer } from '@/components/Markdown/viewer';
-import styles from './index.module.scss';
-import { api } from '@/api';
+import BlogPage from "./BlogPage";
 
-export default async function Page({
-    params,
-}: {
-    params: Promise<{ slug: string }>;
-}) {
+export default async function Page({ params, }: { params: Promise<{ slug: string }>; }) {
     const { slug } = await params;
 
-    let post
-
-    try {
-        const res = await fetch(`${api.articleDateil.detail}/${slug}`, {
-            cache: 'no-store',
-        });
-        const result = await res.json();
-
-        post = result.data;
-    } catch (error) {
-        post = null
-    }
-
-    let carouseUrl = ''
-
-    try {
-
-        const carouselRes = await fetch(`${api.detailHeadPage.scenery}`, {
-            cache: 'no-store'
-        })
-        const carouse = await carouselRes.json()
-        // carouseUrl = carouse.data
-    } catch (error) {
-        carouseUrl = ''
-    }
-
-
-    return (
-        <div className={styles.bolgBox}>
-            <BytemdViewer article={post} carouseUrl={carouseUrl} />
-        </div>
-    );
+    return <BlogPage slug={slug} />
 }
