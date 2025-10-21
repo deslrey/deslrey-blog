9e58019e215095bc6329054c83c78ea4d3bbbde6
deslrey <deslre0381@gmail.com>
2025-10-21 14:19:53 +0800

    feat: Suspense

--- deslrey-web/src/components/Main/index.tsx
+++ deslrey-web/src/components/Main/index.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import styles from "./index.module.scss";
 import LatestReleases from "./LatestReleases";
 import Classify from "./Classify";
 import PopularTags from "./PopularTags";
+import LoaderComponent from "../LoaderComponent";
 
 const Main: React.FC = () => {
     return (
         <div className={styles.main}>
             <div className={styles.latestReleases}>
-                <LatestReleases />
+                <Suspense fallback={<LoaderComponent />}>
+                    <LatestReleases />
+                </Suspense>
             </div>
             <div className={styles.classTags}>
-                <Classify />
-                <PopularTags />
+                <Suspense fallback={<LoaderComponent />}>
+                    <Classify />
+                </Suspense>
+                <Suspense fallback={<LoaderComponent />}>
+                    <PopularTags />
+                </Suspense>
             </div>
         </div>
     );
