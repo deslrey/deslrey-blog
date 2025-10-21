023468f3c0355e35d3e886f63314a87a705c26a3
deslrey <deslre0381@gmail.com>
2025-10-21 14:19:38 +0800

    feat: loader component

---
+++ deslrey-web/src/components/LoaderComponent/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+
+const LoaderComponent = () => {
+    return (
+        <div className="loader-container">
+            <div className="loader">
+                <div className="wrapper">
+                    <div className="circle"></div>
+                    <div className="line-1"></div>
+                    <div className="line-2"></div>
+                    <div className="line-3"></div>
+                    <div className="line-4"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoaderComponent
