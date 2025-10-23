3ddb5360101971a7dde842094e46ad2ade416ffd
deslrey <deslre0381@gmail.com>
2025-10-23 10:46:26 +0800

    fix: padding and avatar.jpg

--- deslrey-web/src/app/about/page.tsx
+++ deslrey-web/src/app/about/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import styles from "./about.module.scss";
 import Image from "next/image";
-import avatar from "@/images/avatar.jpg";
 
 const About = () => {
     return (
         <div className={styles.about}>
             <div className={styles.container}>
                 <Image
-                    src={avatar}
+                    src="/images/avatar.jpg"
                     alt="avatar"
                     className={styles.avatar}
                     width={120}
@@ -19,8 +18,6 @@ const About = () => {
                 <p className={styles.bio}>
                     👋 你好，我是一名前端开发者，我在学习 React、TypeScript 和现代
                     Web 技术。
-                    {/* 👋 你好，我是一名前端开发者，热爱 React、TypeScript 和现代
-                    Web 技术。 */}
                 </p>
             </div>
         </div>
