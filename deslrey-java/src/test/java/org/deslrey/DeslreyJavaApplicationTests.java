c36dcb7feca54878db331ee78539e155c2250cfa
deslrey <deslre0381@gmail.com>
2025-10-30 16:51:17 +0800

    feat: 初始化后端项目

---
+++ deslrey-java/src/test/java/org/deslrey/DeslreyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deslrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeslreyJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
