4379aa0c82106e8b844356bbeefbc90d4a29f8b4
deslrey <deslre0381@gmail.com>
2025-11-08 21:03:24 +0800

    feat: 主页基本布局,移除移动端样式

--- deslrey-web/src/web/pages/HomePage/index.tsx
+++ deslrey-web/src/web/pages/HomePage/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
-import './index.scss'
+import styles from './index.module.scss'
+import LatestReleases from '../../components/HomeComponent/LatestReleases'
+import LoaderComponent from '../../../loader/LoaderComponent'
+import Classify from '../../components/HomeComponent/Classify'
+import PopularTags from '../../components/HomeComponent/PopularTags'
 
 const HomePage: React.FC = () => {
     return (
-        <div>HomePage</div>
+        <div className={styles.homePage}>
+            <div className={styles.latestReleases}>
+                <Suspense fallback={<LoaderComponent />}>
+                    <LatestReleases />
+                </Suspense>
+            </div>
+            <div className={styles.classTags}>
+                <Suspense fallback={<LoaderComponent />}>
+                    <Classify />
+                </Suspense>
+                <Suspense fallback={<LoaderComponent />}>
+                    <PopularTags />
+                </Suspense>
+            </div>
+        </div>
     )
 }
 
