304fa79d294ccccb4c8dde3edf76295c0bad3473
deslrey <deslre0381@gmail.com>
2025-11-09 20:11:49 +0800

    feat: 完成关于界面

--- deslrey-web/src/web/pages/AboutPage/index.tsx
+++ deslrey-web/src/web/pages/AboutPage/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
-import './index.scss'
+import React from "react";
+import styles from "./index.module.scss";
 
 const AboutPage: React.FC = () => {
     return (
-        <div>AboutPage</div>
-    )
-}
+        <div className={styles.about}>
+            <div className={`${styles.container} ${styles.cardDiv}`}>
+                <img
+                    src="/images/avatar.jpg"
+                    alt="avatar"
+                    className={styles.avatar}
+                    width={120}
+                    height={120}
+                />
+                <h2 className={styles.name}>deslrey</h2>
+                <p className={styles.bio}>
+                    👋 你好，我是一名前端开发者，我在学习 React、TypeScript 和现代 Web 技术。
+                </p>
+            </div>
+        </div>
+    );
+};
 
-export default AboutPage
+export default AboutPage;
