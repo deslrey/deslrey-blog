0c1990be2c1eb3b82f3119c1075a4ef382cc96e8
deslrey <deslre0381@gmail.com>
2025-11-18 18:08:47 +0800

    feat: 路由加载中显示加载动画

--- deslrey-web/src/App.tsx
+++ deslrey-web/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { lazy } from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router'
+import TruckLoadComponent from './loader/TruckLoadComponent'
 
-//  web端路由
+// web端路由
 const HomePage = lazy(() => import('./web/pages/HomePage'))
 const ArticlePage = lazy(() => import('./web/pages/ArticlePage'))
 const ArchivePage = lazy(() => import('./web/pages/ArchivePage'))
@@ -10,23 +11,23 @@ const ArticleDetailPage = lazy(() => import('./web/pages/ArticleDetailPage'))
 const CategoryPage = lazy(() => import('./web/pages/CategoryPage'))
 const TagPage = lazy(() => import('./web/pages/TagPage'))
 
-
-//  404
+// 404
 const NotFoundPage = lazy(() => import('./web/pages/NotFoundPage'))
 
 const App: React.FC = () => {
-
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='article' element={<ArticlePage />} />
-      <Route path='archive' element={<ArchivePage />} />
-      <Route path='about' element={<AboutPage />} />
-      <Route path='detail/:id' element={<ArticleDetailPage />} />
-      <Route path='category/:category' element={<CategoryPage />} />
-      <Route path='tag/:tag' element={<TagPage   />} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Routes>
+    <Suspense fallback={<TruckLoadComponent />}>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='article' element={<ArticlePage />} />
+        <Route path='archive' element={<ArchivePage />} />
+        <Route path='about' element={<AboutPage />} />
+        <Route path='detail/:id' element={<ArticleDetailPage />} />
+        <Route path='category/:category' element={<CategoryPage />} />
+        <Route path='tag/:tag' element={<TagPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   )
 }
 
