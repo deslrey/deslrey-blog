c9ecbf65bef8f7d4a3c8bbbb0a67d3a8f1297ca0
deslrey <deslre0381@gmail.com>
2025-12-01 23:30:18 +0800

    fix: 重构分类页面

--- deslrey-web/src/web/pages/CategoryPage/index.tsx
+++ deslrey-web/src/web/pages/CategoryPage/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
-
-import styles from './index.module.scss'
+import React, { useEffect, useState } from 'react';
+import styles from './index.module.scss';
+import request from '../../../utils/reques';
+import { api } from '../../api';
+import type { CountType } from '../../../interfaces';
 
 const CategoryPage: React.FC = () => {
-    return (
-        <div>CategoryPage</div>
-    )
-}
 
-export default CategoryPage
+    const [category, setCategory] = useState<CountType[]>([]);
+
+    const fetchCatagorys = async () => {
+        try {
+            const res = await request.get(api.category.categoryCount);
+            if (res.code === 200) {
+                setCategory(res.data); // 直接设置分类数组
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchCatagorys();
+    }, []);
+
+    return (
+        <div className={styles.categoryPage}>
+            <div className={styles.container}>
+                <h2 className={styles.title}>分类</h2>
+
+                <div className={styles.list}>
+                    {category.map(item => (
+                        <div key={item.id} className={`${styles.card} card-div`}>
+                            <span className={styles.catName}>{item.title}</span>
+                            <span className={styles.count}>共 {item.total} 篇博客</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryPage;
